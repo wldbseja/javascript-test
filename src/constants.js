06761be67e3d90d60e6747ad8217248415b8f8cb
yooni <jiyoon0616@naver.com>
2022-12-16 17:39:10 +0900

    feat (constants) : 출력 문구 const 추가

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+const PRINT_STRING = {
+  OUTPUT_GAME_START: '숫자 맞추기 게임을 시작합니다.',
+  INPUT_MAX_VALUE: '정답 숫자의 최대값을 입력해주세요.(10 이상 1,000 이하)',
+  INPUT_GUESS_NUMBER: '추측한 숫자를 입력해주세요.',
+};
+
+module.exports = { PRINT_STRING };
